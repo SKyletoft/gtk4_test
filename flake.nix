ac3470c05a329e9cb0da240e43e82ccb3a5ad5d0
Samuel Kyletoft <samuel@kyletoft.se>
2022-03-17 00:44:12 +0100

    Alignment and description

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
-	description = "A very basic flake";
+	description = "The relm4 libadwaita demo";
 
 	inputs = {
-		nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+		nixpkgs.url     = "github:nixos/nixpkgs/nixpkgs-unstable";
 		flake-utils.url = "github:numtide/flake-utils";
 	};
 
@@ -11,18 +11,18 @@
 			let pkgs = nixpkgs.legacyPackages.${system};
 			in rec {
 				gtk4_test = pkgs.rustPlatform.buildRustPackage {
-					pname = "gtk4_test";
-					version = "0.0.1";
-					src = self;
-					cargoSha256 = "sha256-ah8IjShmivS6IWL3ku/4/j+WNr/LdUnh1YJnPdaFdcM=";
+					pname              = "gtk4_test";
+					version            = "0.0.1";
+					src                = self;
+					cargoSha256        = "sha256-ah8IjShmivS6IWL3ku/4/j+WNr/LdUnh1YJnPdaFdcM=";
 					cargoLock.lockFile = "${self}/Cargo.lock";
-					nativeBuildInputs = with pkgs; [ pkg-config ];
-					buildInputs = with pkgs; [ gtk4 libadwaita pkg-config ];
+					nativeBuildInputs  = with pkgs; [ pkg-config ];
+					buildInputs        = with pkgs; [ gtk4 libadwaita ];
 				};
 				defaultPackage = gtk4_test;
 				devShell = pkgs.mkShell {
-					shellHook = ''PS1="\e[32;1mnix-flake: \e[34m\w \[\033[00m\]\n↳ "'';
-					buildInputs = gtk4_test.buildInputs;
+					shellHook         = ''PS1="\e[32;1mnix-flake: \e[34m\w \[\033[00m\]\n↳ "'';
+					buildInputs       = gtk4_test.buildInputs;
 					nativeBuildInputs = with pkgs; [ rustup ] ++ gtk4_test.nativeBuildInputs;
 				};
 			}
