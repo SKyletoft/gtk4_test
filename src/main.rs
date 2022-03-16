4a8441215bfd6aa757759f2327c1ff970e46ef11
Samuel Kyletoft <samuel@kyletoft.se>
2022-03-17 00:33:21 +0100

    Probably final state

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
-use relm4::gtk as gtk;
-use relm4::adw::prelude::{BoxExt, ButtonExt, GtkWindowExt};
-use relm4::{send, AppUpdate, Model, RelmApp, Sender, WidgetPlus, Widgets};
+use relm4::{
+	gtk::{
+		self,
+		prelude::{BoxExt, ButtonExt, GtkWindowExt, OrientableExt},
+	},
+	send, AppUpdate, Model, RelmApp, Sender, WidgetPlus, Widgets,
+};
 
+#[derive(Default)]
 struct AppModel {
 	counter: u8,
 }
@@ -31,74 +36,40 @@ impl AppUpdate for AppModel {
 	}
 }
 
-struct AppWidgets {
-	window: gtk::ApplicationWindow,
-	vbox: gtk::Box,
-	inc_button: gtk::Button,
-	dec_button: gtk::Button,
-	label: gtk::Label,
-}
-
+#[relm4_macros::widget]
 impl Widgets<AppModel, ()> for AppWidgets {
-	type Root = gtk::ApplicationWindow;
+	view! {
+		gtk::ApplicationWindow {
+			set_title: Some("Simple app"),
+			set_default_width: 300,
+			set_default_height: 100,
+			set_child = Some(&gtk::Box) {
+				set_orientation: gtk::Orientation::Vertical,
+				set_margin_all: 5,
+				set_spacing: 5,
 
-	/// Initialize the UI.
-	fn init_view(model: &AppModel, _parent_widgets: &(), sender: Sender<AppMsg>) -> Self {
-		let window = gtk::ApplicationWindow::builder()
-			.title("Simple app")
-			.default_width(300)
-			.default_height(100)
-			.build();
-		let vbox = gtk::Box::builder()
-			.orientation(gtk::Orientation::Vertical)
-			.spacing(5)
-			.build();
-		vbox.set_margin_all(5);
-
-		let inc_button = gtk::Button::with_label("Increment");
-		let dec_button = gtk::Button::with_label("Decrement");
-
-		let label = gtk::Label::new(Some(&format!("Counter: {}", model.counter)));
-		label.set_margin_all(5);
-
-		// Connect the widgets
-		window.set_child(Some(&vbox));
-		vbox.append(&inc_button);
-		vbox.append(&dec_button);
-		vbox.append(&label);
-
-		// Connect events
-		let btn_sender = sender.clone();
-		inc_button.connect_clicked(move |_| {
-			send!(btn_sender, AppMsg::Increment);
-		});
-
-		dec_button.connect_clicked(move |_| {
-			send!(sender, AppMsg::Decrement);
-		});
-
-		Self {
-			window,
-			vbox,
-			inc_button,
-			dec_button,
-			label,
+				append = &gtk::Button {
+					set_label: "Increment",
+					connect_clicked(sender) => move |_| {
+						send!(sender, AppMsg::Increment);
+					},
+				},
+				append = &gtk::Button::with_label("Decrement") {
+					connect_clicked(sender) => move |_| {
+						send!(sender, AppMsg::Decrement);
+					},
+				},
+				append = &gtk::Label {
+					set_margin_all: 5,
+					set_label: watch! { &format!("Counter: {}", model.counter) },
+				}
+			},
 		}
-	}
-
-	/// Return the root widget.
-	fn root_widget(&self) -> Self::Root {
-		self.window.clone()
-	}
-
-	/// Update the view to represent the updated model.
-	fn view(&mut self, model: &AppModel, _sender: Sender<AppMsg>) {
-		self.label.set_label(&format!("Counter: {}", model.counter));
 	}
 }
 
 fn main() {
-	let model = AppModel { counter: 0 };
+	let model = AppModel::default();
 	let app = RelmApp::new(model);
 	app.run();
 }
