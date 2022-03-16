5245fb731ebbe70fd50b9338dffb40ec2c6f53a8
Samuel Kyletoft <samuel@kyletoft.se>
2022-03-16 21:55:49 +0100

    Does it only build the lastest commit or what?

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
 	outputs = { self, nixpkgs }:
 		let
 			version = builtins.substring 0 8 self.lastModifiedDate;
-			supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+			supportedSystems = [ "x86_64-linux" "x86-linux" "aarch64-linux" ];
 			forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
 			nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
 		in {
@@ -16,11 +16,9 @@
 						inherit version;
 						src = self;
 						cargoSha256 = "sha256-ah8IjShmivS6IWL3ku/4/j+WNr/LdUnh1YJnPdaFdcM=";
-						cargoLock = {
-							lockFile = "${self}/Cargo.lock";
-						};
+						cargoLock.lockFile = "${self}/Cargo.lock";
 						buildInputs = with pkgs; [ pkgconfig ];
-						nativeBuildInputs = with pkgs; [ gtk4 libadwaita ];
+						nativeBuildInputs = with pkgs; [ gtk4 libadwaita pkgconfig ];
 					};
 				});
 
@@ -33,8 +31,8 @@
 				let pkgs = nixpkgsFor.${system};
 				in pkgs.mkShell {
 					shellHook = ''PS1="\e[32;1mnix-flake: \e[34m\w \[\033[00m\]\n↳ "'';
-					buildInputs = with pkgs; [ rustup ] ++ self.packages.${system}.gtk4_test.buildInputs;
-					nativeBuildInputs = self.packages.${system}.gtk4_test.nativeBuildInputs;
+					buildInputs = with pkgs; [ rustup pkgconfig ];
+					nativeBuildInputs = with pkgs; [ gtk4 libadwaita ];
 				});
 		};
 }
